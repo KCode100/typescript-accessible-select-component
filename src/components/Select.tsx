dfc70806ff598dbe56150cf632cee9508d544290
Kivi Corn <kivi@Kivis-MBP.lan>
2023-06-20 13:41:32 +0100

    REFACTOR: change eventListener to onKeyDown function

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { KeyboardEvent, useEffect, useState } from "react"
 import styles from "./select.module.css"
 
 export type SelectOption = {
@@ -25,7 +25,6 @@ type SelectProps = {
 const Select = ({ multiple, value, onChange, options }: SelectProps) => {
   const [isOpen, setIsOpen] = useState(false)
   const [highlightedIndex, setHighlightedIndex] = useState(0)
-  const containerRef = useRef<HTMLDivElement>(null)
 
   function clearOptions() {
     multiple ? onChange([]) : onChange(undefined)
@@ -51,38 +50,30 @@ const Select = ({ multiple, value, onChange, options }: SelectProps) => {
     if (isOpen) setHighlightedIndex(0)
   }, [isOpen])
 
-  useEffect(() => {
-    const handler = (e: KeyboardEvent) => {
-      if (e.target != containerRef.current) return
-      switch (e.code) {
-        case "Enter":
-        case "Space":
-          setIsOpen(prev => !prev)
-          if (isOpen) selectOption(options[highlightedIndex])
-          break
-        case "ArrowUp":
-        case "ArrowDown": {
-          if (!isOpen) {
-            setIsOpen(true)
-            break
-          }
-          const newValue = highlightedIndex + (e.code === "ArrowDown" ? 1 : -1)
-          if (newValue >= 0 && newValue < options.length) {
-            setHighlightedIndex(newValue)
-          }
+  function handleKeyboardNavigation(e: KeyboardEvent<HTMLDivElement>) {
+    switch (e.code) {
+      case "Enter":
+      case "Space":
+        setIsOpen(prev => !prev)
+        if (isOpen) selectOption(options[highlightedIndex])
+        break
+      case "ArrowUp":
+      case "ArrowDown": {
+        if (!isOpen) {
+          setIsOpen(true)
           break
         }
-        case "Escape":
-          setIsOpen(false)
-          break
+        const newValue = highlightedIndex + (e.code === "ArrowDown" ? 1 : -1)
+        if (newValue >= 0 && newValue < options.length) {
+          setHighlightedIndex(newValue)
+        }
+        break
       }
+      case "Escape":
+        setIsOpen(false)
+        break
     }
-    containerRef.current?.addEventListener("keydown", handler)
-
-    return () => {
-      containerRef.current?.removeEventListener("keydown", handler)
-    }
-  }, [isOpen, highlightedIndex])
+  }
 
   return ( 
       <div
@@ -90,7 +81,7 @@ const Select = ({ multiple, value, onChange, options }: SelectProps) => {
         onClick={() => setIsOpen(prev => !prev)}
         tabIndex={0}
         className={styles.container}
-        ref={containerRef}
+        onKeyDown={e => handleKeyboardNavigation(e)}
       >
         <span className={styles.value}>
           {multiple ? value.map(v => (
