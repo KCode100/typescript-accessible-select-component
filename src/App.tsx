475e244368b05d8b4be026e6759dc3078d4e0027
Kivi Corn <kivi@Kivis-MBP.lan>
2023-06-20 12:43:16 +0100

    complete single select component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import { useState } from "react"
+import Select, { SelectOption } from "./components/Select"
+
+const options = [
+  {label: "first", value: 1},
+  {label: "second", value: 2},
+  {label: "third", value: 3},
+  {label: "fourth", value: 4},
+  {label: "fifth", value: 5},
+]
+
 function App() {
+  const [value, setValue] = useState<SelectOption | undefined>(options[0])
+
   return (
-    <h1>Hello world!</h1>
+    <Select options={options} value={value} onChange={option => setValue(option)} />
   )
 }
 
