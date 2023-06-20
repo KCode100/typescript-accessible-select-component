3a285231de5e4c391a0afdbd4683833ca3f3aac6
Kivi Corn <kivi.webdev@gmail.com>
2023-06-20 13:19:43 +0100

    FEAT: multiple select

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,25 @@ import { useState } from "react"
 import Select, { SelectOption } from "./components/Select"
 
 const options = [
-  {label: "first", value: 1},
-  {label: "second", value: 2},
-  {label: "third", value: 3},
-  {label: "fourth", value: 4},
-  {label: "fifth", value: 5},
+  {label: "First", value: 1},
+  {label: "Second", value: 2},
+  {label: "Third", value: 3},
+  {label: "Fourth", value: 4},
+  {label: "Fifth", value: 5},
 ]
 
 function App() {
-  const [value, setValue] = useState<SelectOption | undefined>(options[0])
+  const [value1, setValue1] = useState<SelectOption | undefined>(options[0])
+  const [value2, setValue2] = useState<SelectOption[]>([options[0]])
 
   return (
-    <Select options={options} value={value} onChange={option => setValue(option)} />
+    <div>
+      <h1 style={{textAlign: 'center'}}>✨ Typescript - React - Accessibility ✨</h1>
+      <h2>Single Select</h2>
+      <Select options={options} value={value1} onChange={option => setValue1(option)} />
+      <h2>Multiple Select</h2>
+      <Select options={options} value={value2} onChange={option => setValue2(option)} multiple />
+    </div>
   )
 }
 
